87b2cd51d2a321c55e2d9454ed71ceefc4d7f321
Danny <dgmmerkx@protonmail.com>
2018-07-19 16:38:54 +0200

    fixed bugs

--- PyTorch/flickr_char/ensemble.py
+++ PyTorch/flickr_char/ensemble.py
@@ -37,7 +37,7 @@ parser.add_argument('-data_loc', type = str, default = '/prep_data/flickr_featur
                     help = 'location of the feature file, default: /prep_data/flickr_features.h5')
 parser.add_argument('-split_loc', type = str, default = '/data/speech2image/preprocessing/dataset.json', 
                     help = 'location of the json file containing the data split information')
-parser.add_argument('-results_loc', type = str, default = '/data/speech2image/PyTorch/flickr_char/ensemble_results/',
+parser.add_argument('-results_loc', type = str, default = '/data/speech2image/PyTorch/flickr_char/ensemble/',
                     help = 'location of the json file containing the data split information')
 # args concerning training settings
 parser.add_argument('-batch_size', type = int, default = 100, help = 'batch size, default: 100')
