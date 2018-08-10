4a5154d66910c5ee81151620e822af3bf072911c
Danny <dgmmerkx@protonmail.com>
2018-08-10 12:46:12 +0200

    dfasdfa

--- PyTorch/functions/trainer.py
+++ PyTorch/functions/trainer.py
@@ -147,7 +147,7 @@ class flickr_trainer():
             test_batches += 1      
             # embed the images and audio using the networks
             img_embedding, cap_embedding, att_matrix = self.embed(img, cap, lengths)
-            loss = self.loss(img_embedding, cap_embedding, cuda = True)
+            loss = self.loss(img_embedding, cap_embedding, self.dtype)
             if self.att_loss:
                 loss += self.att_loss(self.cap_embedder.att, att_matrix, cap_embedding)
             # add loss to average
